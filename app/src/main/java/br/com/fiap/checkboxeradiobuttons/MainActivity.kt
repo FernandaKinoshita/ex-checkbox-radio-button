3115da8052c023d28ebf3fdc23b9a0ae70f5198d
FernandaKinoshita <65926204+FernandaKinoshita@users.noreply.github.com>
2021-11-25 01:18:11 -0300

    primeiro commit

---
+++ app/src/main/java/br/com/fiap/checkboxeradiobuttons/MainActivity.kt
@@ -0,0 +1,93 @@
+package br.com.fiap.checkboxeradiobuttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var chkSandubaGigante : CheckBox
+    lateinit var chkOmelete : CheckBox
+    lateinit var chkHamburguer : CheckBox
+    lateinit var chkHotDog : CheckBox
+    lateinit var chkBeirute : CheckBox
+    lateinit var chkEsfiha : CheckBox
+    lateinit var chkXTudao : CheckBox
+    lateinit var chkVegano : CheckBox
+
+    lateinit var rdgOpcoesPagamento : RadioGroup
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        chkSandubaGigante = findViewById(R.id.chkSandubaGigante)
+        chkOmelete = findViewById(R.id.chkOmelete)
+        chkHamburguer = findViewById(R.id.chkHamburguer)
+        chkHotDog = findViewById(R.id.chkHotDog)
+        chkBeirute = findViewById(R.id.chkBeirute)
+        chkEsfiha = findViewById(R.id.chkEsfiha)
+        chkXTudao = findViewById(R.id.chkXTudao)
+        chkVegano = findViewById(R.id.chkVegano)
+
+        rdgOpcoesPagamento = findViewById(R.id.rdgOpcoesPagamento)
+    }
+
+    fun fazerPedido(view: View) {
+        var total = 0.0
+
+        if ( chkSandubaGigante.isChecked ) {
+            total += 10
+        }
+
+        if ( chkOmelete.isChecked ) {
+            total += 11
+        }
+
+        if ( chkHamburguer.isChecked ) {
+            total += 12
+        }
+
+        if ( chkHotDog.isChecked ) {
+            total += 13
+        }
+
+        if ( chkBeirute.isChecked ) {
+            total += 14
+        }
+
+        if ( chkEsfiha.isChecked ) {
+            total += 15
+        }
+
+        if ( chkXTudao.isChecked ) {
+            total += 16
+        }
+
+        if ( chkVegano.isChecked ) {
+            total += 17
+        }
+
+        val opcaoPagamento = rdgOpcoesPagamento.checkedRadioButtonId
+
+        if ( opcaoPagamento == -1 ) {
+            Toast.makeText(
+                this,
+                getString(R.string.informe_corretamente),
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        if ( opcaoPagamento != R.id.rdbCartaoCredito ) {
+            total *= .85
+        }
+
+        val msg = String.format("%s: R$ %.2f", getString(R.string.total), total)
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}
